9f9f87c23aa529d1850abb9d60cb580f8a9a953d
叶俊权 <425527169@qq.com>
2017-11-21 22:34:57 +0800

    11

---
+++ SocketTest.Common/SocketUtil.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketTest.Common
+{
+    /// <summary>
+    /// Copyright (C) 2017 yjq 版权所有。
+    /// 类名：SocketUtil.cs
+    /// 类属性：公共类（非静态）
+    /// 类功能描述：SocketUtil
+    /// 创建标识：yjq 2017/11/21 22:33:24
+    /// </summary>
+    public static class SocketUtil
+    {
+    }
+}
